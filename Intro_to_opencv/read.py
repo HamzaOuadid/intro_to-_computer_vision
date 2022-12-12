7eabed8bdb61dcd5b35e27c07de9c652fb6b2252
Hamza <94684871+stoichamza@users.noreply.github.com>
2022-12-12 15:21:06 +0100

    Add files via upload

---
+++ Intro_to_opencv/read.py
@@ -0,0 +1,6 @@
+import cv2 as cv
+
+img = cv.imread(r"C:\Users\user\Pictures\yassine-bounou.jpg")
+cv.imshow('ostora', img)
+
+cv.waitKey(0)
---
+++ Intro_to_opencv/realtime_cap.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+# real-time video reading
+cam = cv.VideoCapture(0)
+
+while True:
+    check, frame = cam.read()
+    cv.imshow('video', frame)
+
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv.destroyAllWindows()
